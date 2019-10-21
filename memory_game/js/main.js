467dc62917d5b21161d888457a7d039154bef426
Wootae Yang <wootaeyang@Wootaes-MBP.fios-router.home>
2019-10-21 19:41:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,22 +10,19 @@ console.log("User Flipped " + cardThree);
 */
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	else {
 		alert("Sorry, try again.");
 	}
 }
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID] + ".");
+	cardsInPlay.push(cards[cardID])
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+
