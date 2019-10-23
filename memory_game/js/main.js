e95a16cbe4810652d5073a2ca069cd92f832b529
Wootae Yang <wootaeyang@Wootaes-MBP.fios-router.home>
2019-10-22 22:41:03 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,7 +8,28 @@ var cardFour = "King";
 console.log("User Flipped " + cardOne);
 console.log("User Flipped " + cardThree);
 */
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
@@ -21,9 +42,11 @@ function checkForMatch() {
 	}
 }
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID] + ".");
-	cardsInPlay.push(cards[cardID])
+	console.log("User flipped " + cards[cardID].rank + ".");
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch();
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 }
 flipCard(0);
 flipCard(2);
